1da43a20d01547cdea79f5e38933893abb500de3
Jesus0024 <1034172871@qq.com>
2024-08-05 12:47:01 +0800

    New graph for one stage

--- Function.R
+++ Function.R
@@ -5,7 +5,8 @@
 
 
 
-#-------1. expo_gen---------
+#-------1. expo_gen---------    **** This function is replaced by expo_gen_2stages : interim = 0
+#
 # Generate survival data under exponential dist
 # Return a (N * 3) matrix c(obs_survival, event, arm)
 expo_gen <- function(N,acc_time,lambda,cen_time,arm)
@@ -323,8 +324,8 @@ find_m_t_RMST <- function(m_low, t_low, t_up, rmst_data, search_times, search_st
             if (proc_h0/sim_size > 0 & proc_h0/sim_size < tar_a1 &
                 proc_h1/sim_size >= tar_pow1_low )
               {
-              mark_c <- c(m1,t1,proc_h0/sim_size,proc_h1/sim_size)
-              result_t1 <- cbind(result_t1,mark_c)   
+              mark_c <- c(m1, t1, proc_h0/sim_size, proc_h1/sim_size)
+              result_t1 <- cbind(result_t1, mark_c)   
               }    
         }
       }
